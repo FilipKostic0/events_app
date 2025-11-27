8b0feb83b49b4a0c65b48aaa5ce98bc87295c185
Filip Kostic <fkostic00.w@gmail.com>
2025-11-27 18:00:19 +0100

    added Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
 const Page = () => {
   return (
-    <div>
-    </div>
+    <section>
+      <h1 className="text-center">The Hub for Every Event <br/> You Can't Miss</h1>
+      <p className="text-center mt-5">Parties, Concerts and much more, All in One Place</p>
+      <ExploreBtn/>
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {[1, 2, 3, 4, 5].map((event) => (
+            <li key={event}>Event {event}</li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 };
 
